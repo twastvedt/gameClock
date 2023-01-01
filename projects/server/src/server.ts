31ffb6203abec4efec592d135fb5182e9f5a9316
Trygve Wastvedt <trygve.wastvedt@autodesk.com>
2023-01-01 12:38:44 -0600

    Add history to server?

--- projects/server/src/server.ts
+++ projects/server/src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { join } from "path";
 import { state } from "./state";
 import cors from "cors";
+import history from "connect-history-api-fallback";
 
 dotenv.config({ path: join(__dirname, "../../../.env") });
 
@@ -10,7 +11,7 @@ const production = process.env.NODE_ENV === "production";
 
 const app = express()
   .set("port", process.env.VITE_SERVER_PORT || 3000)
-  .use(json(), cors());
+  .use(json(), cors(), history());
 
 if (production) {
   app.use(
